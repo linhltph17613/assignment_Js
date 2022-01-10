e4d7293efa18cb8f347762599a87d19dcd23bd3c
linhltph17613 <linhlethuy1911@gmail.com>
2022-01-10 21:32:11 +0700

    update main

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import Navigo from "navigo";
-import AboutPage from "./pages/about";
-import HomePage from "./pages/home";
+import Navigo from 'navigo';
 
-const router = new Navigo("/", {linksSelector:"a"});
-const render = (content) => {
-    document.getElementById ("#app").innerHTML = content.print();
+import AboutPage from './pages/about.js';
+import HomePage from './pages/home.js';
 
-    
-}
+const router = new Navigo("/", {linksSelector: "a"});
+const render =  (content) => {
+   // console.log(content); //{print()}
+    document.getElementById("app").innerHTML = content.print();
+};
 
-router.on(
-    {
-        "/": () => {render(HomePage)},
-        "/about": () => {render(AboutPage)}
-    } 
+router.on({
+    "/": () => {
+        render(HomePage);
+    },
+    "/about": () => {
+        render(AboutPage);
 
-)
+    },
+});
 router.resolve();
 
 
